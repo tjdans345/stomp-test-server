4fb984b634aa3964b2b3a5c10f667b25718b9b91
user <tjdans34568@gmail.com>
2022-10-18 10:42:38 +0900

    20221017 = [code] docker + rabbitMQ Test 중 에러 남발하고있음

--- src/main/java/com/example/stomptestserver/config/stomp/StompWebSocketConfig.java
+++ src/main/java/com/example/stomptestserver/config/stomp/StompWebSocketConfig.java
@@ -1,7 +1,8 @@
-package com.example.stomptestserver;
+package com.example.stomptestserver.config.stomp;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,14 +14,22 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // queue : 1:1 , topic : 1:N
-        registry.enableSimpleBroker("/queue","/topic");
-        registry.setApplicationDestinationPrefixes("/pub");
+//        registry.enableSimpleBroker("/queue","/topic");
+        registry.setPathMatcher(new AntPathMatcher(".")) // url 을 chat/room/3 -> chat.room.3으로 참조하기 위한 설정
+                .setApplicationDestinationPrefixes("/pub")
+                .enableStompBrokerRelay("/queue", "/topic", "exchange");
+//                .setRelayHost("localhost")
+//                .setVirtualHost("/")
+//                .setRelayPort(61613)
+//                .setClientLogin("guest")
+//                .setClientPasscode("guest");
+
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
-                .addEndpoint("/ws")
+                .addEndpoint("/stomp/chat")
                 .addInterceptors()
                 .setAllowedOrigins("*");
 //                .withSockJS();
--- src/main/java/com/example/stomptestserver/domain/dto/MessageDTO.java
+++ src/main/java/com/example/stomptestserver/domain/dto/MessageDTO.java
@@ -9,9 +9,16 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MessageDTO {
 
+    // 채팅 타입 ( ENTER (입장), SENDER(보내기) )
     private String type;
+
+    // 보내는 사람
     private String sender;
+
+    // 채팅방 이름 용도
     private String channelId;
+
+    // 메시지 내용
     private Object data;
 
     @Builder
@@ -20,5 +27,8 @@ public class MessageDTO {
         this.sender = sender;
         this.channelId = channelId;
         this.data = data;
+
+        
     }
+
 }
