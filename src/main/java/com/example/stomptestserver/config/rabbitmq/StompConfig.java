1015fd3f080f5251bc533db4cf48fd3d3bfa5d24
user <tjdans34568@gmail.com>
2022-10-19 13:37:23 +0900

    20221017 = [code] docker + rabbitMQ rabbitMQ Connection 안되는 이유 찾기

---
+++ src/main/java/com/example/stomptestserver/config/rabbitmq/StompConfig.java
@@ -0,0 +1,11 @@
+package com.example.stomptestserver.config.rabbitmq;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompConfig {
+
+
+}
