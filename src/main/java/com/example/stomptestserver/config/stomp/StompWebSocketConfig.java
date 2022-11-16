6683e4a89cd06f1ecf26b88f57c527172571dcd3
user <tjdans34568@gmail.com>
2022-11-16 14:25:30 +0900

    20221116 = [code] 주석 추가 , RabbitMQ 연결 작업 진행

--- src/main/java/com/example/stomptestserver/config/stomp/StompWebSocketConfig.java
+++ src/main/java/com/example/stomptestserver/config/stomp/StompWebSocketConfig.java
@@ -31,22 +31,36 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer { 
 
     // stomp 프로토콜 설정
     // 소켓 연결 담당
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // send 요청 처리
+//        registry.setApplicationDestinationPrefixes("/pub");
+        // enableStompBrokerRelay : SimpleBroker 의 기능과 외부 message broker(RabbitMQ, ActiveMQ 등)에 메시지를 전달하는 기능을 가지고 있음.
+        // 외부 메세지 브로커인 RabbitMQ 연결 설정
+//        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue"); // queue : 1:1 , topic : 1:N
+//                .setRelayHost("localhost")
+//                // docker 로 돌릴 때 도커 컨테이너 포트 61613 맞춰 줘야함
+//                // rabbitMQ stomp 설정해줘야함 명령어 : rabbitmq-plugins enable rabbitmq_web_stomp
+//                .setRelayPort(61613) // 메시지 브로커 포트 설정
+//                .setSystemLogin(username)
+//                .setSystemPasscode(password)
+//                .setClientLogin(username)
+//                .setClientPasscode(password);
+//    }
+
+
+    // 아래 코드 참조 블로그 : https://velog.io/@yyong3519/WebSocket-RabbitMQ
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        registry.setPathMatcher(new AntPathMatcher(".")); // url 을 chat/room/3 -> chat.room.3 으로 참조하기 위한 설정
         // send 요청 처리
         registry.setApplicationDestinationPrefixes("/pub");
-        // enableStompBrokerRelay : SimpleBroker의 기능과 외부 message broker(RabbitMQ, ActiveMQ 등)에 메시지를 전달하는 기능을 가지고 있음.
+        // enableStompBrokerRelay : SimpleBroker 의 기능과 외부 message broker(RabbitMQ, ActiveMQ 등)에 메시지를 전달하는 기능을 가지고 있음.
         // 외부 메세지 브로커인 RabbitMQ 연결 설정
-        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue") // queue : 1:1 , topic : 1:N
-                .setRelayHost("localhost")
-                // docker 로 돌릴 때 도커 컨테이너 포트 61613 맞춰 줘야함
-                // rabbitMQ stomp 설정해줘야함 명령어 : rabbitmq-plugins enable rabbitmq_web_stomp
-                .setRelayPort(61613) // 메시지 브로커 포트 설정
-                .setSystemLogin(username)
-                .setSystemPasscode(password)
-                .setClientLogin(username)
-                .setClientPasscode(password);
+        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue"); // queue : 1:1 , topic : 1:N
     }
+
 
 
 
